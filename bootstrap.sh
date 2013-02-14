40b1dc170ccb2adfaaa12590365d5292909af9c2
yannis <excellentbright@gmail.com>
2013-02-14 22:56:59 +0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -30,7 +30,7 @@ for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc; do [ -e $i ] && [ ! -L $i ] && m
 
 if [ ! -e $endpath/.git ]; then
     echo "cloning spf13-vim"
-    git clone --recursive -b 3.0 https://github.com/yannisxu/spf13-vim.git $endpathvim.git $endpath
+    git clone --recursive -b 3.0 https://github.com/yannisxu/spf13-vim.git $endpath
 else
     echo "updating spf13-vim"
     cd $endpath && git pull
